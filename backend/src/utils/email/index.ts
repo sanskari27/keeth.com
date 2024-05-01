5f71e349db338bd68e7903a056a5635a0b7e6b0b
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-05-01 05:57:20 +0530

    Feedback email fixed

--- backend/src/utils/email/index.ts
+++ backend/src/utils/email/index.ts
@@ -5,7 +5,7 @@ const resend = new Resend(RESEND_API_KEY);
 
 export async function sendSimpleText(to: string, value: string) {
 	const { error } = await resend.emails.send({
-		from: 'Feedbacks <no-reply@resend.dev>',
+		from: 'Feedbacks <no-reply@keethjewels.com>',
 		to: [to],
 		subject: 'New Feedback @ keethjewels.com',
 		html: `<p>${value}</p>`,
